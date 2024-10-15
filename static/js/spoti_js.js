dd393b752c78493da17de92187696e61ecb5a99a
Santiago Amaya <santyamaya.98.3.12@gmail.com>
2024-10-15 17:11:13 -0500

    changes in spoti_js

--- static/js/spoti_js.js
+++ static/js/spoti_js.js
@@ -20,36 +20,4 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     });
 };
 
-// Función para manejar el callback de Spotify
-async function callback() {
-    try {
-        const response = await fetch('http://127.0.0.1:8000/home/auth/callback/', {
-            method: 'GET',
-            credentials: 'include',
-        });
 
-        console.log("Response Status:", response.status);
-        console.log("Response Headers:", response.headers);
-
-        // Obtener el texto de la respuesta
-        const text = await response.text();
-        console.log("Response Text:", text); // Imprimir el texto de la respuesta
-
-        // Verifica si la respuesta es correcta
-        if (!response.ok) {
-            throw new Error('Failed to fetch access token');
-        }
-
-        // Analiza el texto de la respuesta como JSON
-        const data = JSON.parse(text);
-        console.log("El token se ha generado con éxito:", data.access_token);
-        
-        // Almacena el token en localStorage
-        localStorage.setItem('access_token', data.access_token);
-        
-        return data.access_token; // Asegúrate de que este sea el campo correcto en tu respuesta
-    } catch (error) {
-        console.error("Error en callback:", error);
-        throw error; // Relanzar el error para que pueda ser capturado más arriba
-    }
-}
